eefb31c550dd41d0f68a17fd805e83937ad79c07
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-14 01:43:29 -0500

    creando middleware para que solos usuarios logeado y con token valido pueda hacer peticiones

--- routers/user.js
+++ routers/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const UserController = require('../controllers/user');
+const md_auth = require("../middleware/authenticated");
 const user = require('../models/user');
 
 const api = express.Router();
 
 api.post('/sign-up', UserController.signUp);
 api.post('/sign-in', UserController.signIn);
-api.get('/get-users', UserController.getUsers);
+api.get('/get-users', [md_auth.ensureAuth], UserController.getUsers);
 
 module.exports = api;
