ff67196d2a3e54ab8f4e0229634ec283e07ae46c
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-26 01:18:14 -0500

    endpoint para obtener todos los menus

--- controllers/menu.js
+++ controllers/menu.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const Menu = require('../models/menu');
 
 async function addMenu(req, res){
@@ -22,6 +23,21 @@ async function addMenu(req, res){
     })
 }
 
+async function getMenus(req, res){
+    await Menu.find().sort({order: "asc"}).exec((err, menusStored) => {
+        if(err){
+            res.status(500).send({message: "Error del servidor"});
+        }else{
+            if(!menusStored){
+                res.status(404).send({message: "No hay menus"});
+            }else{
+                res.status(200).send({menusStored});
+            }            
+        }
+    })
+}
+
 module.exports = {
-    addMenu
+    addMenu,
+    getMenus
 }
