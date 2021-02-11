2ee87a87ddbe1de2781ee8cb56c426820b3d3249
Camilo Vargas <camilovargasingm@gmail.com>
2021-02-11 01:09:00 -0500

    estructura de rutas para crear nuevos post

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const {API_VERSION} = require('./config');
 const authRoutes = require('./routers/auth');
 const userRoutes = require('./routers/user');
 const menuRoutes = require('./routers/menu');
+const postRouters = require('./routers/post');
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
+app.use(`/api/${API_VERSION}`, postRouters);
 
 module.exports = app;
---
+++ controllers/post.js
@@ -0,0 +1,9 @@
+const Post = require("../models/post");
+
+function addPost(req, res){
+    console.log("creando post");
+}
+
+module.exports = {
+    addPost
+};
